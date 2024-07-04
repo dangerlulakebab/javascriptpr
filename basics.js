91ea52aac6697bb9d5d6c753c30c37222a9d9a4d
kebab <101018207+dangerlulakebab@users.noreply.github.com>
2024-07-04 19:05:38 +0300

    Create basics.js

---
+++ basics.js
@@ -0,0 +1,23 @@
+const favColor = 'black';
+console.log(favColor)
+
+currentYear = 2024;
+console.log(currentYear)
+
+const movie = {
+    name: 'spiderman',
+    director: 'niggas',
+    releaseYear: 2012,
+}
+
+console.log(movie.name)
+
+console.log(movie.director)
+
+console.log(movie.releaseYear)
+
+const favBooks = ['spiderman', 'boogieman', 'idk3']
+
+console.log(favBooks[0])
+console.log(favBooks[1])
+console.log(favBooks[2])
