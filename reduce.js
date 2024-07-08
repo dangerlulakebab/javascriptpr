0e9bfd170ee70e996cde676d4ebb77d4ebd2a0e6
kebab <101018207+dangerlulakebab@users.noreply.github.com>
2024-07-08 22:04:51 +0300

    Create reduce.js

---
+++ reduce.js
@@ -0,0 +1,8 @@
+const newNumbers = [1, 3, 5, 7];
+
+const newSum = newNumbers.reduce((accumulator, currentValue) => {
+  console.log('The value of accumulator: ', accumulator);
+  console.log('The value of currentValue: ', currentValue);
+  return accumulator + currentValue;
+}, 10) // 10 is second argument so the beggining accumulator value is 10
+console.log(newSum)
